265f0c0f868fded6051508567a64c1ff65e2c116
Nicole Durham <nicole.martinez.durham@gmail.com>
2023-11-05 15:37:08 -0500

    added weather

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,44 @@ const carousel = new bootstrap.Carousel("#homeCarousel", {
   pause: false,
 });
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById("carouselPause");
-carouselPause.addEventListener("click", function () {
-  console.log("pausing the carousel");
-  carousel.pause();
+const carouselButton = document.getElementById("carouselButton");
+const faIcon = document.getElementById("faButton");
+
+carouselButton.addEventListener("click", function () {
+  if (faIcon.classList.contains("fa-pause")) {
+    faIcon.classList.remove("fa-pause");
+    faIcon.classList.add("fa-play");
+    carousel.pause();
+  } else {
+    faIcon.classList.remove("fa-play");
+    faIcon.classList.add("fa-pause");
+    carousel.cycle();
+  }
 });
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById("carouselPlay");
-carouselPlay.addEventListener("click", function () {
-  console.log("cycle the carousel");
-  carousel.cycle();
-});
+async function fetchWeather() {
+  const apiKey = process.env.OPEN_WEATHER_API_KEY;
+  let city = "Chicago";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayWeather(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchWeather();
+
+function displayWeather(data) {
+  const img = document.createElement("img");
+  img.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let weatherIcon = document.querySelector("#weather-icon");
+  weatherIcon.appendChild(img);
+
+  let temperature = document.querySelector("#weather-temp");
+  temperature.textContent = data.main.temp + "\u00b0";
+}
